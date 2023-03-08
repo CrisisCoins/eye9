9afd47fc1fa5341f842cc55eb97b4d1cece7fcd7
CrisisCoins <github@crisiscoins.com>
2023-03-08 12:18:17 -0800

    Installed NextJS and created the application template

---
+++ src/app/head.tsx
@@ -0,0 +1,10 @@
+export default function Head() {
+    return (
+      <>
+        <title>Eye9</title>
+        <meta content="width=device-width, initial-scale=1" name="viewport" />
+        <meta name="description" content="A Modern News Platform Built Using NextJS 13 and Styled Using Tailwind CSS." />
+        <link rel="icon" href="/favicon.svg" />
+      </>
+    )
+  }
